4be712cb413cc823ea4cf3c29a9383e7e025d42d
Bhavin Rakholiya <rakholiyabhavin99@gmail.com>
2022-03-25 22:10:49 +0530

    Bottom Navigation bar added

--- app/src/main/java/com/practiclewatheraapp/ui/fragment/HomeFragment.kt
+++ app/src/main/java/com/practiclewatheraapp/ui/fragment/HomeFragment.kt
@@ -3,12 +3,20 @@ package com.practiclewatheraapp.ui.fragment
 import com.practiclewatheraapp.R
 import com.practiclewatheraapp.databinding.FragmentHomeBinding
 
-
 class HomeFragment : BaseFragment<FragmentHomeBinding>() {
 
 
     override fun onReady() {
+        mViewDataBinding.bottomNavigation.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.action_home -> {
 
+                }
+                R.id.action_help -> {
+                }
+            }
+            true
+        }
     }
 
     override fun findContentView() = R.layout.fragment_home
